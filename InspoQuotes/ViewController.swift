0d541a38107345db6683945931ec9e91bd150e00
Mel4sa <melisaxsimsek@gmail.com>
2025-05-26 13:44:31 +0300

    Initial Commit

---
+++ InspoQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InspoQuotes
+//
+//  Created by Melisa Şimşek on 26.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
